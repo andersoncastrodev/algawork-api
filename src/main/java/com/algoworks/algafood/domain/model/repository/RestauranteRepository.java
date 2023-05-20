34ef0fb30acbbd7b9a3ae0fb0e4f571c75a25a32
Anderson Castro <andersoncastrosantos@gmail.com>
2023-05-19 21:10:11 -0300

    teste do end ponts

--- src/main/java/com/algoworks/algafood/domain/model/repository/RestauranteRepository.java
+++ src/main/java/com/algoworks/algafood/domain/model/repository/RestauranteRepository.java
@@ -3,9 +3,9 @@ package com.algoworks.algafood.domain.model.repository;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.algoworks.algafood.domain.model.Restaurante;
 
 @Repository
