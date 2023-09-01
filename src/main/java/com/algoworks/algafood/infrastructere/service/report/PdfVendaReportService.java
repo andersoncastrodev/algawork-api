357376382a659aac42aa4dd6572a6874382f25d3
Anderson Castro <andersoncastrosantos@gmail.com>
2023-09-01 10:30:00 -0300

    Relatorio funcionado e salvando PDF

--- src/main/java/com/algoworks/algafood/infrastructere/service/report/PdfVendaReportService.java
+++ src/main/java/com/algoworks/algafood/infrastructere/service/report/PdfVendaReportService.java
@@ -1,17 +1,50 @@
 package com.algoworks.algafood.infrastructere.service.report;
 
+import java.util.HashMap;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algoworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algoworks.algafood.domain.service.VendaQueryService;
 import com.algoworks.algafood.domain.service.VendaReportService;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class PdfVendaReportService implements VendaReportService {
 
+	@Autowired
+	private VendaQueryService vendaQueryService;
+	
 	@Override
 	public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
 		
-		return null;
+		try {
+		//Pegar o arquivo do relatorio.
+		var inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+		
+		//Localizacao
+		var parametros = new HashMap<String , Object>();
+		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+		
+		//Fonte dos Dados.
+		var vendasDiarias = vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+		
+		var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+		
+		var jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+		
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+		
+		} catch (JRException e) {
+			throw new ReportException("Não foi possivel emitir relatorio de vendas diárias",e);
+		}
+		
 	}
 
 }
---
+++ src/main/java/com/algoworks/algafood/infrastructere/service/report/ReportException.java
@@ -0,0 +1,19 @@
+package com.algoworks.algafood.infrastructere.service.report;
+
+public class ReportException extends RuntimeException {
+
+	private static final long serialVersionUID = -3368375578025359427L;
+
+	public ReportException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public ReportException(String message) {
+		super(message);
+		
+	}
+
+	
+	
+}
