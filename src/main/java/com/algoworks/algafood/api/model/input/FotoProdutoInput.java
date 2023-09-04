87a69ac4466ac6af49d2ddfa8857edea278ab23f
Anderson Castro <andersoncastrosantos@gmail.com>
2023-09-04 07:33:16 -0300

    finalizando validacao

--- src/main/java/com/algoworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algoworks/algafood/api/model/input/FotoProdutoInput.java
@@ -1,7 +1,9 @@
 package com.algoworks.algafood.api.model.input;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
-
+import com.algoworks.algafood.core.validation.FileContentType;
+import com.algoworks.algafood.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,7 +14,10 @@ import lombok.Setter;
 public class FotoProdutoInput {
 
 	@NotNull
+	@FileSize(max = "500kb")
+	@FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
 	private MultipartFile arquivo;
+	
 	
 	@NotBlank
 	private String descricao;
