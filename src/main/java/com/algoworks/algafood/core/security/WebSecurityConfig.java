e6538ba8f8aaa1be819f3751de050687c3ec841d
Anderson Castro <andersoncastrosantos@gmail.com>
2024-03-13 06:52:33 -0300

    adicionando spring security

---
+++ src/main/java/com/algoworks/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.algoworks.algafood.core.security;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class WebSecurityConfig extends SecurityConfigurerAdapter {
+	
+	
+}
